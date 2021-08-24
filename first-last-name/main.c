5b954bfde55bbf5035e38c6a03bab37afc513d26
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-24 06:23:51 +0300

    Some simple projects

---
+++ first-last-name/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf ("Bill Clinton\n");
+
+    return 0;
+}
---
+++ name-age-gender/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf ("Brad Pitt\n");
+    printf ("2000 years old\n");
+    printf ("undefined\n");
+
+    printf ("Brad Pitt\n10000 years old\naplha\n");
+
+    return 0;
+}
