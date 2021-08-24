8801fb0eb9b08ea36fb37563a61fb8b98b2af2ff
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2021-08-24 06:33:43 +0300

    Added some more basic projects

--- percent-d/main.c
+++ percent-d/main.c
@@ -4,6 +4,12 @@
 int main() {
 
     printf ("some text %d then som etext %d another text %d and another text %d\n", 1,2,3,4);
+    
+    printf ("some text %d then som etext %d another text %d and another text %d\n", 1.2346446,2,3,4);
+
+    printf ("some text %d then som etext %d another text %d and another text %d\n", 93.7,2,3,4);
+
+    printf ("some text %f then som etext %d another text %d and another text %d\n", 93.7,2,3,4);
 
     return 0;
 }
